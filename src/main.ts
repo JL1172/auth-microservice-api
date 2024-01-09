85a1e85d2182fdce261f2225a27e98f0ceda130c
Jacob Lang <jacoblang11@jacoblang11-REV-1-0.(none)>
2024-01-09 11:40:41 -0500

    added trust proxy configurating to true

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import { AppModule } from './app.module';
 import 'dotenv/config';
 import helmet from 'helmet';
 import { HttpExceptionFilter } from './global-middleware/ErrorMiddleware';
+import { NextFunction, Request, Response } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    req.headers['trust-proxy'] = 'true';
+    next();
+  });
   app.use(helmet());
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(process.env.PORT || 3000);
